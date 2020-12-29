6ca958f7b44a9109662ccfa5a118d86e70d8f454
GC <grzes.cichosz@gmail.com>
2020-12-29 12:15:53 +0100

    lepsze tak

--- topol.ml
+++ topol.ml
@@ -45,6 +45,6 @@ let dfsort g =
       let perm = add v () perm in perm, v::a
   in
   let perm = empty in
-  foldi (fun v vs (perm, a) -> visit perm empty a v) g (perm, []) |> snd
+  foldi (fun v _ (perm, a) -> visit perm empty a v) g (perm, []) |> snd
 
 let topol ls = dfsort (make_graph ls)
