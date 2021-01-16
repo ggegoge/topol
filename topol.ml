95c1c2fa91a6a84c4bacb273941aa4bf8dd7d893
GC <grzes.cichosz@gmail.com>
2021-01-16 16:48:27 +0100

    reviewer info + syntax corrections

--- topol.ml
+++ topol.ml
@@ -1,18 +1,23 @@
-(* topol implementacja -- wersja DFS z pamiętaniem znaczników *)
+(* +------------------- * -------------------+ *)
+(* |----------------- topol -----------------| *)
+(* | autor: Grzegorz Cichosz ----------------| *)
+(* | recenzentka: Katarzyna Mielnik ---------| *)
+(* +------------------- * -------------------+ *)
 
 (* v - vertex, vs - vertices
  * e - edge, es - edges
  * g - graph
- * s - stack 
- * a - accumulator,
+ * a - accumulator
  * m - mark *)
 
 open PMap
 (* graf będzie reprezentowany przez taki słownik -- kluczami są wierzchołki 
  * i są połączone z tymi wierzchołkami, z którymi mają krawędzie. *)
-(* przechowuję też znacznik [mark] mówiący, czy dany wierzchołek był 
- * już odwiedzony [Perm] lub czy jestem w trakcie odwiedzania jego gałęzi [Temp]
- * (pomocne przy wychwytywaniu cykliczności) lub czy nie byłem tam w ogóle [Nil] *)
+(* przechowuję też znacznik (reprezentowany przez typ wariantowy [mark]) 
+ * mówiący, czy dany wierzchołek był: 
+ *  + już odwiedzony [Perm],
+ *  + czy jestem w trakcie odwiedzania jego gałęzi [Temp],
+ *  + lub [Nil] -- jeszcze tam w ogóle nie byłem *)
 type mark = Nil | Temp | Perm
 type 'a graph = ('a, 'a list * mark) PMap.t
 
@@ -21,18 +26,18 @@ exception Cykliczne
 (* z listy na wejściu typu [(a_1,[a_11;...;a_1n]); ...] tworzy graf *)
 let make_graph ls : ('a graph) =
   let grph g (v, es) =
-    let old_es, m =
+    let (old_es, m) =
       try find v g with Not_found -> [], Nil
     in
     add v (es @ old_es, m) g
   in
   List.fold_left grph empty ls
 
-(* sortuję metodą dfsową. oznaczam każdy wierzchołek by wiedzieć, czy już go
- * nie przechodziłem i w ten sposób wychwytuję cykle itp. używam do tego typu mark.
- * visit odwiedza dfsowo i oznacza, a na koniec dodaje do akumulatora i zwraca 
- * zmieniony graf oraz akumulator, który jest końcowym wynikiem sortowania 
- * topologicznego *)
+(* sortuję metodą dfsową. podprocedura visit oznacza [mark] każdy wierzchołek
+ * by wiedzieć, czy już go nie przeszła (lub czy w tej gałęzi właśnie nie jest,
+ * znacznik [Temp] -- wtedy podnoszony jest wyjątek [Cykliczne]).
+ * na koniec dodaje wierzchołek do akumulatora i zwraca zmieniony graf oraz
+ * tenże akumulator, który jest końcowym wynikiem sortowania topologicznego *)
 let dfsort g =
   let rec visit a v g =
     let es, m =
@@ -43,10 +48,10 @@ let dfsort g =
     | Temp -> raise Cykliczne
     | Nil ->
        let g = add v (es, Temp) g in
-       let a, g =
+       let (a, g) =
          List.fold_left (fun (a, g) v -> visit a v g) (a, g) es
        in
-       let g = add v (es, Perm) g in (v::a, g)
+       let g = add v (es, Perm) g in (v :: a, g)
   in
   foldi (fun v _ (a, g) -> visit a v g) g ([], g) |> fst
 
